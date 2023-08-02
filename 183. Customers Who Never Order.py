7f52d156af87d815c977cd46c76fd72631799905
Suraj Sarkar <60427530+benzaminn@users.noreply.github.com>
2023-08-02 15:06:43 +0530

    Update 183. Customers Who Never Order.py

--- 183. Customers Who Never Order.py
+++ 183. Customers Who Never Order.py
@@ -1,3 +1,39 @@
+'''
+Find all customers who never order anything.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Customers table:
++----+-------+
+| id | name  |
++----+-------+
+| 1  | Joe   |
+| 2  | Henry |
+| 3  | Sam   |
+| 4  | Max   |
++----+-------+
+Orders table:
++----+------------+
+| id | customerId |
++----+------------+
+| 1  | 3          |
+| 2  | 1          |
++----+------------+
+Output: 
++-----------+
+| Customers |
++-----------+
+| Henry     |
+| Max       |
++-----------+    '''
+
 import pandas as pd
 
 def find_customers(customers: pd.DataFrame, orders: pd.DataFrame) -> pd.DataFrame:
