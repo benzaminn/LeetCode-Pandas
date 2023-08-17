70a70edd3b5d074f1866f5c3d2c9b08936cb3e90
Suraj Sarkar <60427530+benzaminn@users.noreply.github.com>
2023-08-17 21:04:36 +0530

    Create LeetCode 1484. Group Sold Products By The Date.py

---
+++ LeetCode 1484. Group Sold Products By The Date.py
@@ -0,0 +1,43 @@
+'''
+Input: 
+Activities table:
++------------+------------+
+| sell_date  | product     |
++------------+------------+
+| 2020-05-30 | Headphone  |
+| 2020-06-01 | Pencil     |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | Basketball |
+| 2020-06-01 | Bible      |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | T-Shirt    |
++------------+------------+
+Output: 
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+Explanation: 
+For 2020-05-30, Sold items were (Headphone, Basketball, T-shirt), we sort them lexicographically and separate them by a comma.
+For 2020-06-01, Sold items were (Pencil, Bible), we sort them lexicographically and separate them by a comma.
+For 2020-06-02, the Sold item is (Mask), we just return it.  '''
+
+import pandas as pd
+
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+
+    group = activities.groupby("sell_date")
+
+    stats = group.agg(num_sold=('product','nunique'),products=('product',lambda x : ",".join(sorted(set(x))))).reset_index()
+
+    stats.sort_values("sell_date",inplace=True)
+
+    return stats
+
+
+
+
+
